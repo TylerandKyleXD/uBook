29c70b163879cba31c668447fb34852dfd9bfbb0
TylerandKyleXD <150295600+TylerandKyleXD@users.noreply.github.com>
2025-05-17 20:17:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /// execute.js
 (function() {
   window.addEventListener("keyup", event => {
+    //Control + ~ ||| do JS
     if (event.ctrlKey && event.which === 192) {
       let code = prompt("Eval:");
       if (code.startsWith("javascript:")) {
@@ -8,6 +9,7 @@
       }
       eval(code);
     } else if (event.ctrlKey && event.key === ";") {
+      // Control + ; ||| Portable
       (function() {
         var a = document.getElementById("rusic-modal");
         if (!a) {
@@ -18,6 +20,24 @@
         a.id = "rusic-modal";
         a.style.cssText = "position:fixed;width:100vw;height:100vh;top:0;left:0;right:0;bottom:0;z-index:2147483647;background-color:white;border:none;";
         document.body.appendChild(a);
+      })();
+    } else if (event.ctrlKey && event.shiftKey && event.key === "i") {
+      // Control + Shift + I ||| Open Firebug Devtools
+      (function() {
+        var firebug = document.createElement('script');
+        firebug.setAttribute('src', 'https://luphoria.com/fbl/fbl/firebug-lite-debug.js');
+      
+        document.body.appendChild(firebug);
+      
+        (function() {
+          if (window.firebug && window.firebug.version) {
+            firebug.init();
+          } else {
+            setTimeout(arguments.callee, 100);
+          }
+        })();
+      
+        void(firebug);
       })();
     }
   });
