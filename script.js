58184da8137174b578c385fbdf92f7a81d8f7aa2
TylerandKyleXD <150295600+TylerandKyleXD@users.noreply.github.com>
2025-05-18 14:20:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -21,24 +21,16 @@
         a.style.cssText = "position:fixed;width:100vw;height:100vh;top:0;left:0;right:0;bottom:0;z-index:2147483647;background-color:white;border:none;";
         document.body.appendChild(a);
       })();
-    } else if (event.ctrlKey && event.which === 222) {
-      // Control + ' ||| Open Firebug Devtools
+    } else if (event.ctrlKey && event.key === "i") {
+      // Control + ' ||| Open Web XRay
       alert("active");
       (function() {
-        var firebug = document.createElement("script");
-        firebug.setAttribute("src", "https://luphoria.com/fbl/fbl/firebug-lite-debug.js");
-      
-        document.body.appendChild(firebug);
-      
-        (function() {
-          if (window.firebug && window.firebug.version) {
-            firebug.init();
-          } else {
-            setTimeout(arguments.callee, 100);
-          }
-        })();
-      
-        void(firebug);
+        var a = document.createElement("script");
+        a.src = "https://x-ray-goggles.mouse.org/webxray.js";
+        a.className = "webxray";
+        a.setAttribute("data-lang", "en-US");
+        a.setAttribute("data-baseuri", "https://x-ray-goggles.mouse.org");
+        document.body.appendChild(a);
       })();
     }
   });
