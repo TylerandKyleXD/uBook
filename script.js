8da794675df0ec74820bfc4254605dd0a8e9838c
TylerandKyleXD <150295600+TylerandKyleXD@users.noreply.github.com>
2025-05-22 11:45:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 /// execute.js
 (function() {
+  //inspect element lol
+  (function () {
+    var script = document.createElement('script'); 
+    script.src='//cdn.jsdelivr.net/npm/eruda'; 
+    document.body.appendChild(script); 
+    script.onload = function(){
+      eruda.init()
+    } 
+  })();
   window.addEventListener("keyup", event => {
     //Control + ~ | do JS
     if (event.ctrlKey && event.which === 192) {
